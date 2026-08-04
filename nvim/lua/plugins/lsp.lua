6e97b8817ba2cf65522a0962ebdd61a7428ebd2c
James Ji <57771801+Sannnji@users.noreply.github.com>
2026-08-04 10:13:56 -0400

    add global for lua-lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,11 +2,28 @@ return {
   {
     "neovim/nvim-lspconfig",
     lazy = false,
-    config = function()
+		config = function()
+			vim.lsp.config('lua_ls', {
+        settings = {
+          Lua = {
+            runtime = {
+              version = 'LuaJIT',
+            },
+
+            diagnostics = {
+              globals = {
+                'vim',
+              },
+            },
+          },
+        },
+      })
+
       vim.lsp.enable({
         "lua_ls",
         "pyright",
       })
     end,
+
   },
 }

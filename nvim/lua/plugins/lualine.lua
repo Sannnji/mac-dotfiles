da6d28f543c53351e3f5ccf081870442bb841298
James Ji <57771801+Sannnji@users.noreply.github.com>
2026-08-04 11:58:34 -0400

    update lualine config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,8 +2,36 @@ return {
 	{
 		'nvim-lualine/lualine.nvim',
 		dependencies = { 'nvim-tree/nvim-web-devicons' },
-		config = function()
-			require('lualine').setup({})
-		end
+		opts = {
+			options = {
+				theme = require('shibuya.groups.special.lualine'),
+
+				disabled_filetypes = {
+					statusline = { 'neo-tree', 'codecompanion' },
+					winbar = { 'neo-tree', 'codecompanion' }
+				},
+
+				section_separators = { left = '', right = '' },
+				component_separators = { left = '|', right = '|' },
+			},
+
+			sections = {
+				lualine_a = { 'mode' },
+				lualine_b = { 'branch', 'diff', 'diagnostics' },
+				lualine_c = { 'filename' },
+				lualine_x = { 'encoding', 'fileformat', 'filetype', 'lsp_status' },
+				lualine_y = {  },
+				lualine_z = { 'location' }
+			},
+
+			inactive_sections = {
+				lualine_a = {  },
+				lualine_b = {  },
+				lualine_c = { 'filename' },
+				lualine_x = { 'location' },
+				lualine_y = {  },
+				lualine_z = {  }
+			},
+		},
 	},
 }

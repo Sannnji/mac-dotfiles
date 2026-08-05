3602f77cfcafbbbf588877479c07c1fde23e8b34
James Ji <57771801+Sannnji@users.noreply.github.com>
2026-08-05 11:48:53 -0400

    add keybind for diagnostic float

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,9 +1,10 @@
 return {
   {
-    "neovim/nvim-lspconfig",
+    'neovim/nvim-lspconfig',
     lazy = false,
-		config = function()
-			vim.lsp.config('lua_ls', {
+
+    config = function()
+      vim.lsp.config('lua_ls', {
         settings = {
           Lua = {
             runtime = {
@@ -20,10 +21,24 @@ return {
       })
 
       vim.lsp.enable({
-        "lua_ls",
-        "pyright",
+        'lua_ls',
+        'pyright',
       })
-    end,
 
+      vim.keymap.set(
+				'n',
+				'<leader>d',
+        function ()
+					vim.diagnostic.open_float({ focus = false })
+				end,
+				{ desc = 'Diagnostic Float' }
+			)
+
+			vim.diagnostic.config({
+				float = {
+					border = "rounded",
+				},
+			})
+    end,
   },
 }

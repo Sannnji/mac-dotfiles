4163e1d644663687db9aecf98421b9964d5c8162
James Ji <57771801+Sannnji@users.noreply.github.com>
2026-08-11 10:54:42 -0400

    add plugin

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,67 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+  },
+	opts = {
+		views = {
+			cmdline_popup = {
+				position = {
+					row = 26,
+					col = '50%',
+				},
+				size = {
+					width = 60,
+					height = 'auto',
+				},
+			},
+
+			popupmenu = {
+				enabled = true,
+				relative = 'editor',
+				format = {
+					conceal = false,
+				},
+				position = {
+					row = 29,
+					col = '50%',
+				},
+				size = {
+					width = 60,
+					height = 10,
+				},
+				scrollbar = false,
+				border = {
+					style = 'rounded',
+					padding = { 0, 1 },
+				},
+				win_options = {
+					winhighlight = {
+						Normal = 'Normal',
+					},
+				},
+			},
+		},
+  },
+
+	config = function(_, opts)
+		require('noice').setup(opts)
+
+		-- trigger the wildmenu (and thus noice's popupmenu view) as soon as
+		-- the cmdline is entered/changed, instead of waiting for <Tab>
+		vim.opt.wildmode = 'noselect:lastused,full'
+		vim.opt.wildoptions = 'pum'
+
+		vim.api.nvim_create_autocmd('CmdlineChanged', {
+			pattern = { ':' },
+			callback = function()
+				vim.fn.wildtrigger()
+			end,
+		})
+	end,
+}
---
+++ nvim/lua/plugins/notify.lua
@@ -0,0 +1,9 @@
+return {
+	"rcarriga/nvim-notify",
+	opts = {
+		fps = 30,
+		timeout = 5000,
+		stages = "fade_in_slide_out", -- fade | slide | slide_out | fade_in_slide_out | static
+		top_down = true,
+	},
+}

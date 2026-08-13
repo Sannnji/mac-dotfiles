234105f25d37594b2db44256c21d64097710db1e
James Ji <57771801+Sannnji@users.noreply.github.com>
2026-08-13 12:21:45 -0400

    add auto-refresh cmd on closing lazygit

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -50,6 +50,17 @@ return {
 				end,
 			})
 
+			-- auto refresh neo-tree after exiting lazygit
+			vim.api.nvim_create_autocmd({ "BufLeave" }, {
+				pattern = { "*lazygit*" },
+				group = vim.api.nvim_create_augroup("git_refresh_neotree", {clear = true}),
+				callback = function()
+					require("neo-tree.sources.filesystem.commands").refresh(
+						require("neo-tree.sources.manager").get_state("filesystem")
+					)
+				end,
+			})
+
 		end,
 	},
 }

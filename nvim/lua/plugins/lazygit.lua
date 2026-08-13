08f2250ee01152296fe5a631c801b519af38a17a
James Ji <57771801+Sannnji@users.noreply.github.com>
2026-08-13 12:17:30 -0400

    add lazygit plugin

---
+++ nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,24 @@
+return {
+	"kdheepak/lazygit.nvim",
+	lazy = true,
+	cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+	},
+	-- optional for floating window border decoration
+	dependencies = {
+			"nvim-lua/plenary.nvim",
+	},
+	-- remove float border entirely
+	-- init = function()
+	-- 	vim.g.lazygit_floating_window_border_chars = "none"
+	-- end,
+	-- setting the keybinding for LazyGit with 'keys' is recommended in
+	-- order to load the plugin when the command is run for the first time
+	keys = {
+			{ "<leader>l", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+	}
+}
